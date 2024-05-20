c1961a1e49e1df9d0eca64b1a63a42cb674a7969
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2024-05-20 22:12:41 +0300

    create split bill counter

---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: BillSpitter(),
+  ));
+}
+
+class BillSpitter extends StatefulWidget {
+  const BillSpitter({super.key});
+
+  @override
+  State<BillSpitter> createState() => _BillSpitterState();
+}
+
+class _BillSpitterState extends State<BillSpitter> {
+  final int _tipPercentage = 0;
+  int _personCounter = 1;
+  double _billAmount = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.all(20.5),
+        children: [
+          Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+                color: Colors.purpleAccent.shade400.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(12.5)),
+            child: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("Total per Person"), Text("\$123")],
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 20.0),
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                border: Border.all(
+                  color: Colors.blueGrey.shade100,
+                  style: BorderStyle.solid,
+                ),
+                borderRadius: BorderRadius.circular(12.0)),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  style: const TextStyle(color: Colors.grey),
+                  decoration: const InputDecoration(
+                      prefixText: "Bill amount",
+                      prefixIcon: Icon(Icons.attach_money)),
+                  onChanged: (String value) {
+                    try {
+                      _billAmount = double.parse(value);
+                    } catch (exception) {
+                      _billAmount = 0.0;
+                    }
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Split",
+                      style: TextStyle(color: Colors.grey.shade700),
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              if (_personCounter > 1) {
+                                _personCounter -= 1;
+                              } else {}
+                            });
+                          },
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            margin: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                                color: Colors.purpleAccent.shade100
+                                    .withOpacity(0.2)),
+                            child: const Center(
+                              child: Text(
+                                "-",
+                                style: TextStyle(
+                                    color: Colors.purple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("$_personCounter", style: const TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15
+                        ),),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              _personCounter += 1;
+                            });
+                          },
+                          child: Container(
+                            width: 40,
+                            height: 40,
+                            margin: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                                color: Colors.purpleAccent.shade100
+                                    .withOpacity(0.2)),
+                            child: const Center(
+                              child: Text(
+                                "+",
+                                style: TextStyle(
+                                    color: Colors.purple,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17.0),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+}
